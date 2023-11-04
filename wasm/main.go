ac0dfe873d5787e82adeeef47934d9b08d539df7
ManuelTello <manuelTello.business@gmail.com>
2023-11-04 16:47:42 -0700

    Changes in the wasm project.

--- wasm/main.go
+++ wasm/main.go
@@ -1,5 +1,3 @@
-//go:build js && wasm
-
 package main
 
 import (
@@ -7,15 +5,29 @@ import (
 	"syscall/js"
 )
 
-func registerCallbacks(){
-    js.Global().Set("Click",js.FuncOf(handleClick))
+func registerCallbacks() {
+	js.Global().Set("Click", js.FuncOf(handleClick))
 }
 
-func handleClick(this js.Value,inputs []js.Value)interface{}{
-    println("Button clicked")
-    return nil
+func handleClick(this js.Value, inputs []js.Value) interface{} {
+	println("Button clicked")
+	return nil
 }
 
-func main(){
-    fmt.Println("Hello world")
+func main() {
+	registerCallbacks()
+
+	var document js.Value = js.Global().Get("document")
+	var body js.Value = document.Get("body")
+
+	var p js.Value = document.Call("createElement", "p")
+	p.Set("innerHTML", "Hola")
+
+	body.Call("appendChild", p)
+
+	c := make(chan struct{}, 0)
+
+	<-c
+
+	fmt.Println("Hello world")
 }
